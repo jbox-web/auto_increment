7d86d119d51581d47c926395207c1825aaaa1bdc
Nicolas Rodriguez <nico@nicoladmin.fr>
2024-11-12 00:27:51 +0100

    Cleanup spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,9 +11,5 @@ end
 ENV['RAILS_ENV'] = 'test'
 require File.expand_path('dummy/config/environment.rb', __dir__)
 
-# Load test gems
-require 'rspec/rails'
-require 'database_cleaner'
-
 # Load our own config
 require_relative 'config_rspec'
